45553b9ad7920aa8a76be4a80d7d8e938e1d2ca2
bsaw <brendan_saw@sfu.ca>
2021-02-21 09:38:08 -0800

    Got Modal submission started

--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, {  useState  } from 'react';
 import hlogo from './logov2.png'; // gives image path
-import { Navbar, Nav } from 'react-bootstrap';
+import { Modal, Navbar, Nav, Button} from 'react-bootstrap';
 import './Header.css';
-const Header = () => {
+
+function Header () {
+    const [lgShow, setLgShow] = useState(false);
+    const [modalTitle, setModalTitle] = useState(false);
+    const [modalBody, setModalBody] = useState(false);
+    let handleClickHeader = () =>  {
+        console.log("New topic clicked");
+        setLgShow(true);
+        setModalTitle("new title");
+        setModalBody("new body");
+
+    };
+
+    let handleSubmitNew = () => {
+        console.log("submitted data");
+        setLgShow(false)
+    }
+
     return (
         <div>
             <div className="header">
                 <Navbar bg="primary" expand="lg">
                     <div className="container">
-                        <Navbar.Brand href="#home"><img src={hlogo} alt="Logo Image" /></Navbar.Brand>
+                        <Navbar.Brand href=""><img src={hlogo} alt="Logo Image" /></Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="ml-auto">
-                                <Nav.Link href="#new_topic"><h1>New Topic</h1></Nav.Link>
+                                <Nav.Link href="" onClick={() => handleClickHeader()}><h1>New Topic</h1></Nav.Link>
                                 
                             </Nav>
                         </Navbar.Collapse>
                     </div>
                 </Navbar>
             </div>
+
+            <Modal
+                size="xl"
+                show={lgShow}
+                onHide={() => setLgShow(false)}
+                aria-labelledby="example-modal-sizes-title-lg"
+            >
+                <Modal.Header closeButton>
+                <Modal.Title id="example-modal-sizes-title-lg">
+                    New Thread: {modalTitle}
+                </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{modalBody}</Modal.Body>
+                <Modal.Footer>
+                <Button variant="primary" onClick={() => handleSubmitNew()}>
+                    Submit
+                </Button>
+                </Modal.Footer>
+
+            </Modal>
         </div>
     );
 }
+
 
 export default Header;
