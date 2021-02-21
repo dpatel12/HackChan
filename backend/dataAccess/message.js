bb8d34c518cac93e1202789acac3a226f61d444f
Liversticks <55716846+Liversticks@users.noreply.github.com>
2021-02-20 23:09:43 -0800

    Support POST and PUT requests :D

--- backend/dataAccess/message.js
+++ backend/dataAccess/message.js
@@ -1,6 +1,3 @@
-/*
-DEPRECATED - use DB directly in controllers
-
 const db = require('../db');
 // callbacks :)
 
@@ -20,7 +17,7 @@ module.exports = {
       timestamp: createdAt,
       parentThreadTime: createdAt
     };
-    
+    */
     let queryString = `INSERT INTO comment_thread VALUES (${threadParent.firstCreatedAt}, ${threadParent.lastCreatedAt}, '${threadParent.title}', 1); INSERT INTO comment_entry VALUES (${threadChild.timestamp}, '${threadChild.body}', ${threadChild.parentThreadTime});`;
     db.pool.query(queryString)
       .then(res => {
@@ -39,7 +36,7 @@ module.exports = {
       timestamp: createdAt,
       parentThreadTime: req.body.parentTime
     }
-    
+    */
     let queryString = `INSERT INTO comment_entry VALUES (${threadComment.timestamp}, '${threadComment.body}', ${threadComment.parentThreadTime}); UPDATE comment_thread SET number_of_comments = number_of_comments + 1 WHERE init_com_time = ${threadComment.parentThreadTime});`;
     db.pool.query(queryString)
       .then(res => {
@@ -53,62 +50,33 @@ module.exports = {
   getThreadsByLatest: (params) => {
 
     let maxCount = params.maxCount;
-
       let queryString = `SELECT * FROM comment_thread ORDER BY init_com_time DESC LIMIT ${maxCount};`;
       console.log(queryString);
 
       db.pool.query(queryString)
         .then(res => {
-          console.log(res.rows + "\n" + typeof(res.rows));
+          console.log(res.rows);
           return res.rows;
         })
         .catch(err => {
           console.error(err);
           throw "Could not retrieve threads.";
         });
-
-    let queryString = `SELECT * FROM comment_thread ORDER BY init_com_time DESC LIMIT ${maxCount};`;
-    console.log(queryString);
-
-    db.query(queryString)
-      .then(res => {
-        console.log(res.rows);
-        return res.rows;
-      })
-      .catch(err => {
-        console.error(err);
-        throw "Could not retrieve threads.";
-      });
-
   },
 
   getEntriesByLatest: (params, parent_time) => {
 
     let maxCount = params.maxCount;
     let queryString = `SELECT * FROM comment_entry WHERE parent_init_time = ${parent_time} ORDER BY comment_time DESC LIMIT ${maxCount};`;
-
     db.pool.query(queryString)
       .then(res => {
         console.log(res.rows + "\n" + typeof(res.rows));
-
-    
-
-    
-    db.query(queryString)
-      .then(res => {
-        console.log(res.rows);
-
         return res.rows;
       })
       .catch(err => {
         console.error(err);
-
         throw "Could not retrieve entries.";
-
-        throw "Could not retrieve threads.";
-
       });
   }
 
 }
-*/
