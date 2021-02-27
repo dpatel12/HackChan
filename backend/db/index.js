4369ac586c9e9dd7e83064283fd2995fb01e085e
Liversticks <55716846+Liversticks@users.noreply.github.com>
2021-02-27 14:06:49 -0800

    use connection string if present

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,14 +1,14 @@
 const { Pool } = require('pg');
 const dotenv = require('dotenv').config();
 //const cs = "postgresql://postgres:droptablesallday@localhost:5432/hackchan";
-const pool = new Pool({
+const pool = process.env.DATABASE_URL === null ? new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_ADDRESS,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT
-
-   //process.env.DATABASE_URL || process.env.LOCALDATABASE
+}) : new Pool({
+  connectionString: process.env.DATABASE_URL
 });
 
 module.exports = {
